02d989051215d7a27e082db91f466c7d5694f992
Claudio Romero <clau_sr511@hotmail.com>
2023-12-13 23:58:36 -0300

    ruters

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
-import express, { response } from "express"
-import ProductManager from "./controllers/ProductManager.js"
-
-const product = new ProductManager()
-
+import express from "express"
+import ProductRouter from "./router/product.routes.js"
 
 
 const app = express()
@@ -11,46 +8,7 @@ const PORT = 8080
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-// no se pórque no funciona GET
-app.get("/products", async (req, res) => {
-    res.send(await product.getProducts())
-})
-
-//
-app.get("/products/:id", async (req, res) => {
-    let id = req.params.id
-    res.send(await product.getProductsById(id))
-})
-
-app.post("/products", async (req, res) => {
-
-    let newProduct = req.body
-
-    res.send(await product.addProducts(newProduct))
-
-})
-
-
-//actualizar un producto
-
-app.put("/products/:id", async (req, res) => {
-    let id = req.params.id
-    let updateProduct = req.body
-    res.send(await product.updateProduct(id, updateProduct))
-
-
-})
-
-
-
-//Eliminar productos
-app.delete("/products/:id", async (req, res) => {
-    let id = req.params.id
-    res.send(await product.deleteProducts(id))
-
-})
-
+app.use("/products", ProductRouter)
 
 app.listen(PORT, () => {
     console.log(`Servidor Express Puerto N° ${PORT}`)
